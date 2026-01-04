a43a7e6dde3be69a14328a0026de1620fac7870f
Sirine Amrane <sirine.amrane@outlook.com>
2026-01-04 16:06:39 +0100

    ptx assembly

---
+++ inlineptx.cu
@@ -0,0 +1,7 @@
+__device__ float fma_ptx(float a, float b, float c) {
+    float result;
+    asm("fma.rn.f32 %0, %1, %2, %3;"
+        : "=f"(result)
+        : "f"(a), "f"(b), "f"(c));
+    return result;
+}
